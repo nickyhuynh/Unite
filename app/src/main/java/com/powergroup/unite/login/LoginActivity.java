3df16725785fbefd9e36447a3cd24157c3c3ff58
Nicky Huynh <nh.nicky.huynh@gmail.com>
2017-04-02 07:33:12 -0700

    getting random account

--- app/src/main/java/com/powergroup/unite/login/LoginActivity.java
+++ app/src/main/java/com/powergroup/unite/login/LoginActivity.java
@@ -53,8 +53,7 @@ public class LoginActivity extends GenericActivity {
             @Override
             public void onSuccess(LoginResult loginResult) {
                 Log.d(TAG, "Logged in!");
-//                navigateToCreateProfile();
-                navigateToMain();
+                navigateToCreateProfile();
             }
 
             @Override
--- app/src/main/java/com/powergroup/unite/unite/UnifyFragment.java
+++ app/src/main/java/com/powergroup/unite/unite/UnifyFragment.java
@@ -14,7 +14,9 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.powergroup.unite.R;
+import com.powergroup.unite.app.Profile;
 import com.powergroup.unite.chat.ChatActivity;
+import com.powergroup.unite.main.MainActivity;
 
 /**
  * Created by bummy on 4/1/17.
@@ -24,6 +26,7 @@ public class UnifyFragment extends Fragment {
 
     private TextView gender;
     private TextView bio;
+    private String id;
 
     private TextView reject;
     private TextView accept;
@@ -56,7 +59,16 @@ public class UnifyFragment extends Fragment {
         FirebaseDatabase.getInstance().getReference().child("/profiles").addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                
+                int randomNum = 0 + (int)(Math.random() * dataSnapshot.getChildrenCount());
+                int i = 0;
+                for(DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    if(i == randomNum) {
+                        Profile.ProfileInfo info = snapshot.getValue(Profile.ProfileInfo.class);
+                        gender.setText(info.gender);
+                        bio.setText(info.bio);
+                        id = info.id;
+                    }
+                }
             }
 
             @Override
@@ -67,6 +79,36 @@ public class UnifyFragment extends Fragment {
     }
 
     private void assignHandlers() {
+        reject.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseDatabase.getInstance().getReference().child("/profiles").addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        int randomNum = 0 + (int)(Math.random() * dataSnapshot.getChildrenCount());
+                        int i = 0;
+                        for(DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                            if(i == randomNum) {
+                                Profile.ProfileInfo info = snapshot.getValue(Profile.ProfileInfo.class);
+                                gender.setText(info.gender);
+                                bio.setText(info.bio);
+                            }
+                        }
+                    }
 
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+            }
+        });
+
+        accept.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ((MainActivity)getActivity()).navigateToChat(Profile.INSTANCE.info.id, id);
+            }
+        });
     }
 }
